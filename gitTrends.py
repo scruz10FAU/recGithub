c3f34a8638a74e730916bafb1840c8ef87880c5d
Samantha <scruz10@fau.edu>
2025-10-20 18:45:11 -0400

    added total stars and stars today

---
+++ gitTrends.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def request_github_tranding(url):
+    return requests.get(url).text
+
+def extract(page):
+    soup = BeautifulSoup(page, 'html.parser')
+    return soup.find_all('article')
+
+def transform(repos_html):
+    result=[]
+    for r in repos_html:
+        number_of_stars = ''.join(r.select_one('a.Link.Link--muted.d-inline-block.mr-3').text.split())
+        stars_today = ''.join(r.select_one('span.float-sm-right').text.split())
+        stars_today = number_of_stars.replace("starstoday", "")
+        repository_name = ''.join(r.select_one('h2').text.split()) 
+        developer_name = r.select_one('img.avatar.mb-1.avatar-user')['alt']
+        language = r.select_one('span[itemprop="programmingLanguage"]').text.strip() if r.select_one('span[itemprop="programmingLanguage"]') else "Unknown"
+        result.append({'Developer': developer_name, 'Repository Name': repository_name, 'Number of Stars': number_of_stars, 'Stars Today': stars_today, 'Language': language})
+    return result
+
+def format(repositories_data):
+    columns = ['Developer, Repository_ame, Number of Stars', 'Stars Today', 'Language']
+    for repository in repositories_data:
+        r = [repository['Developer'], repository['Repository Name'], repository['Number of Stars'], repository['Stars Today'], repository['Language']]
+        columns.append(','.join(r))
+    return '\n'.join(columns) 
+
+def out_to_csv(repository_data, filename):
+    df = pd.DataFrame(repository_data)
+    df.to_csv(filename, index=False)
+
+
+def main():
+    url = "https://github.com/trending"
+    page = request_github_tranding(url)
+    repos_html = extract(page)
+    #print(repos_html)
+    repo_data = transform(repos_html)
+    #print(repo_data)
+    print(format(repo_data))
+    #out_to_csv(repo_data, "trending.csv")
+
+if __name__ == "__main__":
+    main()
